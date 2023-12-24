ef80048a69e8009e36cd977b4c67cc064e345982
scherepanov <Serhii.Cherepanov.CONTRACTOR@bydeluxe.com>
2023-12-24 05:32:43 +0400

    step-ca update

--- Formula/digitalspace-step-ca.rb
+++ Formula/digitalspace-step-ca.rb
@@ -17,10 +17,31 @@ class DigitalspaceStepCa < Formula
       nil
   end
 
+  def supervisor_config 
+    step_path = `#{Formula["step"].opt_bin}/step path --base`.strip
+    <<~EOS
+    [program:step-ca]
+      command=#{Formula["step"].opt_bin}/step-ca #{step_path}/config/ca.json --resolver=127.0.0.1 --password-file=#{etc}/step-ca-password
+      directory=#{opt_prefix}
+      stdout_logfile=#{var}/log/digitalspace-supervisor-step-ca.log
+      stdout_logfile_maxbytes=1MB
+      stderr_logfile=#{var}/log/digitalspace-supervisor-step-ca.err
+      stderr_logfile_maxbytes=1MB
+      #user=
+      autorestart=true
+      stopasgroup=true
+    EOS
+  rescue StandardError
+    nil
+  end
+
   def install
     (buildpath / "bin" / "digitalspace-step-ca-init").write(script_step_ca_init)
     (buildpath / "bin" / "digitalspace-step-ca-init").chmod(0755)
     bin.install "bin/digitalspace-step-ca-init"
+
+    (buildpath / "etc" / "digitalspace-supervisor.d" / "step-ca.ini").write(supervisor_config)
+    pkgetc.install buildpath / "etc" / "digitalspace-supervisor.d"
   end
 
   step_path = `#{Formula["step"].opt_bin}/step path --base`.strip
@@ -34,26 +55,15 @@ class DigitalspaceStepCa < Formula
   end
 
   def post_install
+    inreplace pkgetc/"digitalspace-supervisor.d"/"step-ca.ini" do |s|
+      s.gsub! "#user=", "user=#{ENV['USER']}", false
+    end
+
+    (Formula["digitalspace-supervisor"].etc/"digitalspace-supervisor.d").install_symlink pkgetc / "digitalspace-supervisor.d" / "step-ca.ini"
+
     step_path = `#{Formula["step"].opt_bin}/step path --base`.strip
-
-    supervisor_config =<<~EOS
-      [program:step-ca]
-      command=#{Formula["step"].opt_bin}/step-ca #{step_path}/config/ca.json --resolver=127.0.0.1 --password-file=#{etc}/step-ca-password
-      directory=#{opt_prefix}
-      stdout_logfile=#{var}/log/digitalspace-supervisor-step-ca.log
-      stdout_logfile_maxbytes=1MB
-      stderr_logfile=#{var}/log/digitalspace-supervisor-step-ca.err
-      stderr_logfile_maxbytes=1MB
-      user=#{ENV['USER']}
-      autorestart=true
-      stopasgroup=true
-    EOS
-
+    
     (etc/"step-ca-password").write((0...8).map { (65 + rand(26)).chr }.join) if !(etc/"step-ca-password").exist?
-
-    (etc/"digitalspace-supervisor.d").mkpath
-    (etc/"digitalspace-supervisor.d"/"step-ca.ini").delete if (etc/"digitalspace-supervisor.d"/"step-ca.ini").exist?
-    (etc/"digitalspace-supervisor.d"/"step-ca.ini").write(supervisor_config)
 
     system("#{Formula["step"].opt_bin}/digitalspace-step-ca-init") unless File.exist?("#{step_path}/config/ca.json")
   end
