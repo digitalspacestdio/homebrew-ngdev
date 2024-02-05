9e8e703e61f9ff6c5adc6d3d1521a31f6e9ae752
scherepanov <sergey@cherepanov.org.ua>
2024-02-05 17:41:12 +0400

    postgresql15

--- Formula/digitalspace-postgresql15.rb
+++ Formula/digitalspace-postgresql15.rb
@@ -1,7 +1,7 @@
 class DigitalspacePostgresql15 < Formula
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  version "0.1.1"
+  version "0.1.2"
 
   depends_on 'postgresql@15'
 
@@ -71,7 +71,7 @@ class DigitalspacePostgresql15 < Formula
   end
 
   service do
-    run [Formula["postgresql@15"].opt_bin/"postgres", "-D", postgresql_data_dir]
+    run [Formula["postgresql@15"].opt_bin/"postgres", "-D", f.postgresql_data_dir]
     environment_variables LC_ALL: "C"
     keep_alive true
     require_root false
