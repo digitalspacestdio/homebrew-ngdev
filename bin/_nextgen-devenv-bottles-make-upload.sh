8d6137501464833ecc916b1d810b1a06f0e1c291
scherepanov <sergey@cherepanov.org.ua>
2024-02-09 23:00:15 +0000

    revisions

--- bin/_nextgen-devenv-bottles-make-upload.sh
+++ bin/_nextgen-devenv-bottles-make-upload.sh
@@ -4,7 +4,11 @@ if [[ ! -z $DEBUG ]]; then set -x; fi
 pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
 cd "${DIR}"
 
-FORMULAS=$(brew search digitalspacestdio/nextgen-devenv | grep "$1\|$1@[0-9]\+" | awk -F'/' '{ print $3 }' | sort)
+if [ -z $1 ]; then
+    FORMULAS=$(brew search digitalspacestdio/nextgen-devenv | awk -F'/' '{ print $3 }' | sort)
+else
+    FORMULAS=$(brew search digitalspacestdio/nextgen-devenv | grep "$1\|$1@[0-9]\+" | awk -F'/' '{ print $3 }' | sort)
+fi
 
 ./_nextgen-devenv-bottles-make.sh $FORMULAS && {
     if [[ -z $NO_UPLOAD ]];  then
