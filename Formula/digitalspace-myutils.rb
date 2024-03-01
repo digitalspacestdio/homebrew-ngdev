3e72bd7f95d90ad9f33990dd924406ed5a608866
scherepanov <sergey@cherepanov.org.ua>
2024-03-01 23:59:39 +0400

    myutils

---
+++ Formula/digitalspace-myutils.rb
@@ -0,0 +1,23 @@
+class DigitalspaceAllutils < Formula
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "0.1.0"
+
+  depends_on "coreutils"
+  depends_on "moreutils"
+  depends_on "gettext"
+  depends_on "mtr"
+  depends_on "htop"
+  depends_on "jq"
+  depends_on "yq"
+  depends_on "pv"
+  depends_on "s3cmd"
+  depends_on "jenv"
+  depends_on "nvm"
+  
+
+  def install
+    (buildpath / ".keepme").write("")
+    prefix.install ".keepme"
+  end
+end
