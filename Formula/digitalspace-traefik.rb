5e8a538d3fa8fc04d8cd70f4ae2be4cb0547bd33
S.Cherepanov <963777+sergeycherepanov@users.noreply.github.com>
2024-06-07 02:32:45 +0400

    Update digitalspace-traefik.rb

--- Formula/digitalspace-traefik.rb
+++ Formula/digitalspace-traefik.rb
@@ -1,7 +1,7 @@
 class DigitalspaceTraefik < Formula
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  version "0.1.3"
+  version "0.1.4"
 
   depends_on "traefik@2"
   depends_on "digitalspace-local-ca"
