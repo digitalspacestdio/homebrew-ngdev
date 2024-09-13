2ac7d648d281b9b370c65a2be40f227153e580a6
scherepanov <s@cherepanov.co>
2024-09-13 16:16:09 +0000

    107

--- bin/_ngdev-bottles-make.sh
+++ bin/_ngdev-bottles-make.sh
@@ -32,8 +32,8 @@ do
         for FORMULA in $FORMULAS; do
             find "${HOMEBREW_PREFIX}/etc" -maxdepth 1 -name "${FORMULA//"$TAP_NAME_PREFIX"/}*" -exec rm -v -rf {} \; || true
             if [[ -n $REBUILD ]]; then
-                brew list | grep '^'${FORMULA//"$TAP_NAME_PREFIX"/}'$' | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} brew uninstall --force --ignore-dependencies {}
-                brew deps --full --direct $FORMULA | grep "${TAP_NAME}" | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} brew uninstall --force --ignore-dependencies {}
+                brew list | grep '^'${FORMULA//"$TAP_NAME_PREFIX"/}'$' | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} bash -c 'brew uninstall --force --ignore-dependencies {} || true'
+                brew deps --full --direct $FORMULA | grep "${TAP_NAME}" | xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} bash -c 'brew uninstall --force --ignore-dependencies {} || true'
                 rm -rf ${HOME}/.bottles/$FORMULA.bottle
             fi
             for DEP in $(brew deps --full --direct $FORMULA | grep "${TAP_NAME}"); do
@@ -71,7 +71,7 @@ do
             echo "==> Building bottles for $FORMULA ..."
             [[ "true" == $(brew info --json=v1 $FORMULA | jq '.[0].installed[0].built_as_bottle') ]] || {
                 echo "==> Removing previously installed formula $FORMULA ..."
-                brew list | grep '^'${FORMULA//"$TAP_NAME_PREFIX"/}'$' && xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} brew uninstall --force --ignore-dependencies {}
+                brew list | grep '^'${FORMULA//"$TAP_NAME_PREFIX"/}'$' && xargs $(if [[ "$OSTYPE" != "darwin"* ]]; then printf -- '--no-run-if-empty'; fi;) -I{} bash -c 'brew uninstall --force --ignore-dependencies {} || true'
             }
 
             FORMULA_REVISION=$(brew info --json=v1 $FORMULA | jq '.[0].revision')
