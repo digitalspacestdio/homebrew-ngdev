26f0e248d88bbcf72e7e528adc435045c7a67f4c
Serhii Cherepanov <scherepanov@macos-monteray.digitalspace.local>
2024-09-26 01:32:09 +0400

    bottles update

--- Formula/digitalspace-mysql@8.4.rb
+++ Formula/digitalspace-mysql@8.4.rb
@@ -13,6 +13,7 @@ class DigitalspaceMysqlAT84 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/109/digitalspace-mysql@8.4"
+    sha256 cellar: :any_skip_relocation, ventura:      "6cdf3f5770d6a69dd2b01992fd2809f7ad676df48045385c449e237507429d92"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "a1ffd0f963b5864ed633677141b051acea69dff5cd2272c11e1d9eec947d9991"
   end
 
