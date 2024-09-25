caa39a5b4e6a0f09da1cf841f570e4500cc1157d
Serhii Cherepanov <scherepanov@macos-monteray.digitalspace.local>
2024-09-26 01:43:09 +0400

    bottles update

--- Formula/digitalspace-supervisor.rb
+++ Formula/digitalspace-supervisor.rb
@@ -12,6 +12,7 @@ class DigitalspaceSupervisor < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/109/digitalspace-supervisor"
+    sha256 cellar: :any_skip_relocation, ventura:      "a798c0754da8a38d7b1a795b5ea3b5bc43a06984f511b0a7697e913b63584618"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5a07ff180acfb9a329e079c9a22abf257cfdf429a05371985cb4db9b1405e5c7"
   end
 
