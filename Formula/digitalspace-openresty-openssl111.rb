059d96d2ba13c38692b8fc447d2a42bb3905f5fe
scherepanov <s@cherepanov.co>
2024-09-27 01:13:15 +0000

    bottle digitalspace-openresty-openssl111 linux-gnu

--- Formula/digitalspace-openresty-openssl111.rb
+++ Formula/digitalspace-openresty-openssl111.rb
@@ -4,9 +4,8 @@ class DigitalspaceOpenrestyOpenssl111 < Formula
   revision 110
 
   bottle do
-    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/109/digitalspace-openresty-openssl111"
-    sha256 cellar: :any_skip_relocation, ventura:      "b4493c299b8c60e9f25f92f5e9225727781c79c8108ebd7c7a69bca2a3ffa147"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b06004d49932d13035eda66d5f8b2feda6800bc37dd30db91f83ccff010277c0"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-openresty-openssl111"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a109bb7077dac977eb1ef8a5553790913702ec171a6b20421d18f11232e9b137"
   end
 
   VERSION = "1.1.1n".freeze
