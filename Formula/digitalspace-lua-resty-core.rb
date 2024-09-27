df2959f59c666a8a95135aaa2a8627d1744e89e4
Serhii Cherepanov <scherepanov@macos-monteray.digitalspace.local>
2024-09-27 12:57:16 +0400

    bottle digitalspace-lua-resty-core darwin22

--- Formula/digitalspace-lua-resty-core.rb
+++ Formula/digitalspace-lua-resty-core.rb
@@ -8,6 +8,7 @@ class DigitalspaceLuaRestyCore < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-lua-resty-core"
+    sha256 cellar: :any_skip_relocation, ventura:      "7e8b00c5a1a4839f353f92eda94bc442193f66bc5e32df5028395fa6074c937d"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "9114c7df85d50c668c8a5698f5c9852e72e4e997fa2648632fa588df906d1578"
   end
 
