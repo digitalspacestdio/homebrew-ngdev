bb8608537adde978e837c1affc578909e46dca37
scherepanov <s@cherepanov.co>
2024-09-27 00:23:25 +0000

    bottle digitalspace-allutils linux-gnu

--- Formula/digitalspace-allutils.rb
+++ Formula/digitalspace-allutils.rb
@@ -5,9 +5,8 @@ class DigitalspaceAllutils < Formula
   revision 110
 
   bottle do
-    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/109/digitalspace-allutils"
-    sha256 cellar: :any_skip_relocation, ventura:      "1352316fc00ea86e0957561c1cbeb31ec3152ab2677d1da68edc7b745feda3dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ae444b46f5aa1cf45695e8b4f19761d8fe38dd4b6a6005877f58d3f124be0905"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-allutils"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "dea0c8ebbb5a3c832b860eb0406e1739df0a7476175528b08c148eddbe8aa26c"
   end
 
   depends_on "coreutils"
