fbb58910b5e90e00278b523faa6d160ac47420b4
scherepanov <s@cherepanov.co>
2024-09-28 17:23:04 +0400

    bottle digitalspace-mysql@5.7 darwin22

--- Formula/digitalspace-mysql@5.7.rb
+++ Formula/digitalspace-mysql@5.7.rb
@@ -8,8 +8,9 @@ class DigitalspaceMysqlAT57 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-mysql@5.7"
-    sha256 cellar: :any_skip_relocation, ventura:      "76391c3cc15010b288b858e3409305b23d3681dec83b4d306f611cc1a0fcd811"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ea5d4593d3bf783e64c9fe490d6093afd17c6b9860ed8696d2f333a99d62b436"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a9b913692687d63dac0fe54f550faeaf3f889f8b0d6d7e74ebb6a83beb128053"
+    sha256 cellar: :any_skip_relocation, ventura:       "76391c3cc15010b288b858e3409305b23d3681dec83b4d306f611cc1a0fcd811"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea5d4593d3bf783e64c9fe490d6093afd17c6b9860ed8696d2f333a99d62b436"
   end
 
   keg_only :versioned_formula
