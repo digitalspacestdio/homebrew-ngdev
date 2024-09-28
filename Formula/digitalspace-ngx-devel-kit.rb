e82d31a699f68dec3bc407d2f046aaa8ce1c5a9d
scherepanov <s@cherepanov.co>
2024-09-28 20:18:09 +0400

    bottle digitalspace-ngx-devel-kit darwin22

--- Formula/digitalspace-ngx-devel-kit.rb
+++ Formula/digitalspace-ngx-devel-kit.rb
@@ -8,8 +8,9 @@ class DigitalspaceNgxDevelKit < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-ngx-devel-kit"
-    sha256 cellar: :any_skip_relocation, ventura:      "0a9e99a7169db3603ed073881b91be21963cac56027f910078668b427bfa5602"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "140ae046a073804967639938263a0ac6a7597a3d904c1380ff8ac532b0ce3b87"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "12192199123b9623410f650f55ece0a8aa50104f0507a3a87f2a83e03f75939e"
+    sha256 cellar: :any_skip_relocation, ventura:       "0a9e99a7169db3603ed073881b91be21963cac56027f910078668b427bfa5602"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "140ae046a073804967639938263a0ac6a7597a3d904c1380ff8ac532b0ce3b87"
   end
   
   def install
