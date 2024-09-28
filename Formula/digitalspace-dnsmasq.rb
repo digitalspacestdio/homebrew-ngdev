202665d16db01807c83e1b8e68c10ac789359136
scherepanov <s@cherepanov.co>
2024-09-28 17:17:36 +0400

    bottle digitalspace-dnsmasq darwin22

--- Formula/digitalspace-dnsmasq.rb
+++ Formula/digitalspace-dnsmasq.rb
@@ -13,8 +13,9 @@ class DigitalspaceDnsmasq < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-dnsmasq"
-    sha256 cellar: :any_skip_relocation, ventura:      "0feacd6339aaf9418b17dc20df671c233dc82d17f88bd0f4b5d53a7237bf0dff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4305c206540cfe97bef960b8c59d1032dddd9b4a47509dd45961c94f08be8af9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "893550ad0a72fbe2fbfdb19ddf6f795bc948800a0161a9f7b22f78638baefe4e"
+    sha256 cellar: :any_skip_relocation, ventura:       "0feacd6339aaf9418b17dc20df671c233dc82d17f88bd0f4b5d53a7237bf0dff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4305c206540cfe97bef960b8c59d1032dddd9b4a47509dd45961c94f08be8af9"
   end
 
   depends_on "pkg-config" => :build
