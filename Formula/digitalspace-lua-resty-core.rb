e8e7aa48728ac21c02b246f264482a4c9d5b9850
scherepanov <s@cherepanov.co>
2025-01-20 21:37:15 +0000

    bottle digitalspace-lua-resty-core linux-gnu

--- Formula/digitalspace-lua-resty-core.rb
+++ Formula/digitalspace-lua-resty-core.rb
@@ -7,11 +7,8 @@ class DigitalspaceLuaRestyCore < Formula
   revision 111
 
   bottle do
-    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/110/digitalspace-lua-resty-core"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "9dfd8e6f465d2de5005c9e588ee6624a6172552af0414753acf541cedde7dfe1"
-    sha256 cellar: :any_skip_relocation, ventura:       "269b5e4530988563a6d8d2b3a925195e74abfcdae2692baeae65b98c27c7d420"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49bdfa5b86ec20dc8cf6786ea70807045b99195e789e352c610b400b437fb5b4"
-    sha256 cellar: :any_skip_relocation, aarch64_linux: "3ba36b5dcb8664ae32880423674697447efcc43bc67a451d11e7753d9731e045"
+    root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/111/digitalspace-lua-resty-core"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c958e22836fcc0381105fac91ea916c04eeb6956f053e27d01390f61a338c21c"
   end
 
   def install
