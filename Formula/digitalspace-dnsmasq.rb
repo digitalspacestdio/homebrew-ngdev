750333fe478fd0de1684aa594752f8203954e3d5
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-22 16:42:55 +0400

    bottle digitalspace-dnsmasq darwin22

--- Formula/digitalspace-dnsmasq.rb
+++ Formula/digitalspace-dnsmasq.rb
@@ -13,7 +13,8 @@ class DigitalspaceDnsmasq < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/111/digitalspace-dnsmasq"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b11a88ca4b74313c2f1aa79752bcee57267292f538e39d1f14e9975b1369f3d2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e965c47297b51fbc698e71f96a77b27a11efc715b0799e1076df572ac46f1e14"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b11a88ca4b74313c2f1aa79752bcee57267292f538e39d1f14e9975b1369f3d2"
   end
 
   depends_on "pkg-config" => :build
