c0e51b5e5bbbdd1eb57b81e62dad246a368dbcb5
scherepanov <s@cherepanov.co>
2025-02-22 18:07:17 +0400

    bottle digitalspace-mysql@8.0 darwin22

--- Formula/digitalspace-mysql@8.0.rb
+++ Formula/digitalspace-mysql@8.0.rb
@@ -14,6 +14,7 @@ class DigitalspaceMysqlAT80 < Formula
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/111/digitalspace-mysql@8.0"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "6b6354935169d582134b6d63fddd995055132f31f9bfaca3ba8b2c3f41042e9f"
+    sha256 cellar: :any_skip_relocation, ventura:       "c106b5d4961eea1b43886349f290ab4c847576313451e4a1c823d972cbaa7c66"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "11b4089bb95c54b49df22cc493009a2e96b901a2d68231c449665fb117a5d97c"
   end
 
