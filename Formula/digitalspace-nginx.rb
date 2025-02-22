92d73f61a242916294a5ea943a60ee1b348ddf0f
scherepanov <s@cherepanov.co>
2025-02-22 21:08:41 +0000

    nginx fastcgi_hide_header X-Forwarded-For;

--- Formula/digitalspace-nginx.rb
+++ Formula/digitalspace-nginx.rb
@@ -459,7 +459,7 @@ class DigitalspaceNginx < Formula
           fastcgi_param                   MAGE_IS_DEVELOPER_MODE true;
           fastcgi_param                   SERVER_NAME $host;
           fastcgi_param                   HTTPS $fcgi_https;
-          fastcgi_hide_header X-Forwarded-For;
+          fastcgi_param                   HTTP_X_FORWARDED_FOR $http_empty;
       }
       EOS
 rescue StandardError
