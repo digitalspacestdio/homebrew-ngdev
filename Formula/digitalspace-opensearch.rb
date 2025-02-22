b3d17a429397484bd9013ab6549c5888ebba44d2
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-22 22:31:48 +0400

    bottle digitalspace-opensearch darwin22

--- Formula/digitalspace-opensearch.rb
+++ Formula/digitalspace-opensearch.rb
@@ -6,7 +6,9 @@ class DigitalspaceOpensearch < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/111/digitalspace-opensearch"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d184512bbd4213399c9b5e36f0d3cad28959700306a597176f36b99871a13d78"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9fba17d75105ad87fbc89958c6528696b875e7d181b7101d5164242c779ecb82"
+    sha256 cellar: :any_skip_relocation, ventura:       "4da963235f8959331657d970c50b191637e0de1f4bc35b52b19b961f9f3e4753"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d184512bbd4213399c9b5e36f0d3cad28959700306a597176f36b99871a13d78"
   end
 
   depends_on 'opensearch'
