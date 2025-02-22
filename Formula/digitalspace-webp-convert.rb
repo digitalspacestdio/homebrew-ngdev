57578cd7b6e00dddf091d496c7b17a2e52df44dc
scherepanov <s@cherepanov.co>
2025-02-22 18:57:02 +0400

    bottle digitalspace-webp-convert darwin22

--- Formula/digitalspace-webp-convert.rb
+++ Formula/digitalspace-webp-convert.rb
@@ -7,6 +7,7 @@ class DigitalspaceWebpConvert < Formula
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/111/digitalspace-webp-convert"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "dae901f56ef50f168f825a1f5edf2efd7de7ab21511939b87977b6803d52e84f"
+    sha256 cellar: :any_skip_relocation, ventura:       "59e33e343981a6a1bd8748158f37a0eeb6b34da266c61f3004d17740a34ca181"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "ffb03d9d8bd91b8489de35a7589bb6c54d776555f3e9d1c814254d11c30ed8bc"
   end
 
