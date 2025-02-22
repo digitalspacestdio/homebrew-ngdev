1ac73a146b3be2bcd11275706d8988961bc9e0e0
serhii.cherepanov <serhii.cherepanov@paysera.net>
2025-02-22 17:17:28 +0400

    bottle digitalspace-postgresql15 darwin22

--- Formula/digitalspace-postgresql15.rb
+++ Formula/digitalspace-postgresql15.rb
@@ -13,7 +13,8 @@ class DigitalspacePostgresql15 < Formula
 
   bottle do
     root_url "https://pub-7d898cd296ae4a92a616d2e2c17cdb9e.r2.dev/ngdev/111/digitalspace-postgresql15"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a0ede06188a67d5aac83b3053d04f35e8dc4142f3a2f36aa68da19ceeca2a823"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "831bb8b850808b616e0c92ca760ab96056e6840979f57e865533bdfbf175a110"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0ede06188a67d5aac83b3053d04f35e8dc4142f3a2f36aa68da19ceeca2a823"
   end
 
   depends_on "pkg-config" => :build
