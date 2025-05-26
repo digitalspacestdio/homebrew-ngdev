dc9d7228813a980f3a4579ff345aac796e5d4b26
scherepanov <s@cherepanov.co>
2025-05-26 19:20:31 +0000

    dnsmasq update

--- Formula/digitalspace-dnsmasq.rb
+++ Formula/digitalspace-dnsmasq.rb
@@ -314,6 +314,8 @@ class DigitalspaceDnsmasq < Formula
       begin
           inreplace etc / "digitalspace-dnsmasq.conf" do |s|
             s.sub!(/^.*?listen-address=.*$/, "listen-address=127.0.1.1")
+            s.sub!(/^.*?no-resolv.*$/, "no-resolv")
+            s.sub!(/^.*?server=.*$/, "server=1.1.1.1")
           end
 
           bin_path = HOMEBREW_PREFIX/"bin/digitalspace-dnsmasq-start"
